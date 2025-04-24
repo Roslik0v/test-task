0e2189c1e557744f4524a63c5c6c27c9d3d196cb
Roslik0v <roslikovsemen@gmail.com>
2025-04-24 13:49:47 +0700

    hooked up Laravel

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('/', 'form');
+
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
