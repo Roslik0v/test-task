fdbeb2f97a336bbb06d314c9b1b4ed01a9903ddc
Roslik0v <roslikovsemen@gmail.com>
2025-04-24 16:55:28 +0700

    binded admin_email to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton('admin_email', function () {
+            return config('custom.admin_email');
+        });
     }
 
     /**
